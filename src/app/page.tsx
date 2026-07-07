00a63d952b45013bb980549cfa325f1ffb924f98
Justin Lee <justinlee0777@gmail.com>
2026-07-07 17:32:13 -0400

    Initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,65 @@
-import Image from "next/image";
+import './homepage.css';
+
+import { useMemo } from 'react';
+import Image from 'next/image';
+
+interface Project {
+  description: string;
+  header: string;
+  thumbnail: string;
+  url: string;
+  imageAlt: string;
+}
 
 export default function Home() {
+  const projects: Array<Project> = useMemo(() => {
+    return [
+      {
+        header: 'Author Map',
+        thumbnail: '/projects/author-map.gif',
+        description: 'Project for documenting American literary history.',
+        imageAlt: 'A gif depicting the interactive author map.',
+        url: '/author-map',
+      },
+      {
+        header: 'Prospero',
+        thumbnail: '/projects/prospero.gif',
+        description: 'Render text on the web as a book.',
+        url: '/prospero',
+        imageAlt: 'A gif of flipping pages through Prospero.',
+      },
+      {
+        header: 'Picture-in-Picture JS',
+        thumbnail: '/projects/picture-in-picture-js.gif',
+        description: 'Render media on a webpage in a draggable overlay.',
+        url: '/picture-in-picture-js',
+        imageAlt:
+          'A gif of the picture-in-picture-js creating an overlay and moving said overlay.',
+      },
+      {
+        header: 'Buzzword Bingo',
+        thumbnail: '/projects/buzzword-bingo.png',
+        description:
+          'Library that takes in bingo cells and renders them as a bingosheet.',
+        url: '/buzzword-bingo',
+        imageAlt: 'A static image of a Buzzword Bingo sheet.',
+      },
+    ];
+  }, []);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="stuff">
+      <div className="projects">
+        {projects.map(({ header, thumbnail, description, url, imageAlt }) => {
+          return (
+            <a key={header} className="project" href={url}>
+              <Image width={200} height={150} src={thumbnail} alt={imageAlt} />
+              <h4>{header}</h4>
+              <p>{description}</p>
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
